219e60edc69ee0264cdcd0f3cc4557f44f17897a
wvdbrande <wim.vandenbrande@one-agency.be>
2017-06-14 15:53:56 +0200

    First working version -with campaigns retrieved from HTTP request!

--- RC5_Template/app/app.module.ts
+++ RC5_Template/app/app.module.ts
@@ -2,6 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { HttpModule } from '@angular/http';
+// urls of angular2-data-table:
+// https://www.npmjs.com/package/angular2-data-table
+// https://swimlane.gitbooks.io/ngx-datatable/content/
+// https://swimlane.gitbooks.io/ngx-datatable/content/introduction/installing.html
+// https://swimlane.gitbooks.io/ngx-datatable/content/introduction/getting-started.html
+// https://github.com/swimlane/ngx-datatable/blob/master/demo/basic/scrolling.component.ts
+//import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @NgModule({
   imports:      [ BrowserModule, HttpModule ],
--- RC5_Template/app/campaign-list.component.ts
+++ RC5_Template/app/campaign-list.component.ts
@@ -7,34 +7,72 @@ import { CampaignService} from './campaign.service';
                 <table border="2px">
                     <thead>
                     <tr>
+                        <th>Id</th>
                         <th>Name</th>
                         <th>Description</th>
+                        <th>Owner</th>
                         <th>Creation Time</th>
+                        <th>Enabled</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Asset</th>
+                        <th>Selector</th>
                     </tr>
                     </thead>
                     <tr *ngFor="let campaign of rows">
+                        <td align="center">{{campaign.id}}</td>
                         <td>{{campaign.name}}</td>
                         <td>{{campaign.description}}</td>
-                        <!--<td>{{campaign.creationTime | date:'dd/MM/yyyy HH:mm:ss'}}</td>-->
+                        <td>{{campaign.owner}}</td>
                         <td>{{campaign.creationTime}}</td>
+                        <td align="center">{{campaign.enabled == true ? 'Yes' : 'No'}}</td>
+                        <td>{{campaign.startTime}}</td>
+                        <td>{{campaign.endTime}}</td>
+                        <td>
+                            <table border="1px">
+                                <tr *ngFor="let asset of campaign.assets">
+                                    <td>{{asset.id}}</td>
+                                    <td>{{asset.name}}</td>
+                                    <td>{{asset.filename}}</td>
+                                    <td>{{asset.duration}}</td>
+                                    <td>{{asset.index}}</td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <table border="1px" >
+                                <tr *ngFor="let selector of campaign.selectors">
+                                    <td>{{selector.id}}</td>
+                                    <td>{{selector.name}}</td>
+                                    <!--<td>{{selector.description}}</td>-->
+                                    <td>{{selector.priority}}</td>
+                                    <td>{{selector.maxViews}}</td>
+                                    <td>{{selector.coolDownPeriod}}</td>
+                                    <td>{{selector.enabled == true ? 'Yes' : 'No'}}</td>
+                                    <!--<td>{{selector.startTime}}</td>-->
+                                    <!--<td>{{selector.endTime}}</td>-->
+                                    <!--<td>{{selector.rule}}</td>-->
+                                </tr>
+                            </table>
+                        </td>
                     </tr>
                 </table>`
-    //template: `<h2>Campaign Overview</h2>
-    //                 <ngx-datatable
-    //                    class="material"
-    //                    [rows]="rows"
-    //                    columnMode="force"
-    //                    [headerHeight]="50"
-    //                    [footerHeight]="0"
-    //                    [rowHeight]="50"
-    //                    [scrollbarV]="true">
-    //                    <ngx-datatable-column name="Name" [width]="300" prop="name"></ngx-datatable-column>
-    //                    <ngx-datatable-column name="Description" prop="description"></ngx-datatable-column>
-    //                    <ngx-datatable-column name="Created" prop="creationTime"></ngx-datatable-column>
-    //                    <ngx-datatable-column name="Owner" [width]="300" prop="owner"></ngx-datatable-column>
-    //                    <ngx-datatable-column name="Start" [width]="300" prop="startTime"></ngx-datatable-column>
-    //                    <ngx-datatable-column name="End" [width]="300" prop="endTime"></ngx-datatable-column>
-    //                 </ngx-datatable>`
+  //  template: `<h2>Campaign Overview</h2>
+  //                   <ngx-datatable
+  //                      class="material"
+  //                      [rows]="rows"
+  //                      columnMode="force"
+  //                      [headerHeight]="50"
+  //                      [footerHeight]="0"
+  //                      [rowHeight]="50"
+  //                      [scrollbarV]="true">
+  //                      <ngx-datatable-column name="Name" [width]="300" prop="name"></ngx-datatable-column>
+  //                      <ngx-datatable-column name="Description" prop="description"></ngx-datatable-column>
+  //                      <ngx-datatable-column name="Created" prop="creationTime"></ngx-datatable-column>
+  //                      <ngx-datatable-column name="Owner" [width]="300" prop="owner"></ngx-datatable-column>
+  //                      <ngx-datatable-column name="Start" [width]="300" prop="startTime"></ngx-datatable-column>
+  //                      <ngx-datatable-column name="End" [width]="300" prop="endTime"></ngx-datatable-column>
+  //                   </ngx-datatable>`
 })
 
 export class CampaignListComponent implements OnInit{
